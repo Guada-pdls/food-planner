22713807031ebd50e513e61f95de837b1fbf6929
Agustín Montesdeoca <agustinmontesdeoca38@gmail.com>
2025-06-09 10:39:20 -0300

    Realice primer acercamiento a la estructura de la encuesta, agregue el import en global

--- src/app/survey/page.js
+++ src/app/survey/page.js
@@ -2,10 +2,29 @@ import React from 'react'
 
 const page = () => {
   return (
-    <form>
-        
+    <form id='formularioEncuesta' className="flex flex-col ">
+      <input type="number" placeholder="Edad" className=""  />
+      <input type="number" placeholder="Peso" className="input" />
+      <input type="number" placeholder="Altura" className="input" />
+    <label className="select">
+      <span className="label">Actividad Fisica</span>
+      <select>
+        <option></option>
+        <option>Sedentarismo/Ninguna</option>
+        <option>Baja</option>
+        <option>Moderada</option>
+        <option>Alta</option>
+      </select>
+    </label>
+
+
     </form>
+
+
+
   )
 }
 
 export default page
+
+// edad peso altura actividad fisica

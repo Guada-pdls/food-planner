31730c59c94a4ef2ce8998982a8997c0ef785715
Agustín Montesdeoca <agustinmontesdeoca38@gmail.com>
2025-06-11 08:42:32 -0300

    Realice estructura formulario

--- src/app/survey/page.js
+++ src/app/survey/page.js
@@ -1,27 +1,38 @@
 import React from 'react'
 
+const inputClass = "input";
+const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted with name:');
+};
+
+
 const page = () => {
+
+  
+
   return (
-    <form id='formularioEncuesta' className="flex flex-col ">
-      <input type="number" placeholder="Edad" className=""  />
-      <input type="number" placeholder="Peso" className="input" />
-      <input type="number" placeholder="Altura" className="input" />
-    <label className="select">
-      <span className="label">Actividad Fisica</span>
-      <select>
-        <option></option>
-        <option>Sedentarismo/Ninguna</option>
-        <option>Baja</option>
-        <option>Moderada</option>
-        <option>Alta</option>
-      </select>
-    </label>
+    <div className="h-screen flex justify-center items-center "> 
+      <form id='formularioEncuesta' className="flex flex-col ml-100px bg-white border-2 border-red-600 p-8 space-y-4" onSubmit={handleSubmit}>
+        <input type="number" placeholder="Edad" min={1} className={inputClass}   />
+        <input type="number" placeholder="Peso" min={1} className={inputClass} />
+        <input type="number"  min={1} placeholder="Altura" className={inputClass} />
+      <label className="select">
+        <span className="label">Actividad Fisica</span>
+        <select>
+          <option></option>
+          <option>Sedentarismo/Ninguna</option>
+          <option>Baja</option>
+          <option>Moderada</option>
+          <option>Alta</option>
+        </select>
+      </label>
 
-
-    </form>
-
-
-
+      <input type="submit" placeholder="Altura" className="input bg-green-600" />
+      
+      </form>
+   </div>
+    
   )
 }
 
